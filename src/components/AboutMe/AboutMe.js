955dc7ad62dc8b8f2a632b0721fa2ecb07142422
danji90 <danji_ma90@hotmail.com>
2019-07-20 12:46:55 +0200

    first commit

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './AboutMe.scss'
+
+function AboutMe() {
+
+  return (
+    <div className="about-container">
+      <h1>About Me</h1>
+      <p>Hi, I'm Daniel! I am from Bolzano, a small town in the Italian Alps. Bolzano province is bilingual and a melting pot between Italian and Austrian culture due to its history. My father is British, so one could say I am a half-British, German-speaking Italian, moulded by a multicultural and multilingual environment.</p>
+      <p>In the professional and academic world I describe myself as a passionate geo-information researcher, developer and tech lover. With an environmental sciences and geographical technology background, my main interests lie in nature, environmental monitoring, sensors, open data and open hardware, the Internet of Things, interoperability and geospatial web development.</p>
+      <p>I have travelled and lived in several countries and continents, and would include my open-mindedness and friendly nature to my core skills. My multilingual background and my travels contributed to my advanced language skills. Fluent in four languages and currently learning more, I count linguistics as a hobby and skill alike. I enjoy different kinds of sports, and having grown up in the alps, I love mountaineering and outdoor activities. Currently you'll find me mostly rock climbing and mountain biking in my free time. My artistic side lies in my passion for the guitar and music in general, which I have been developing since I was 14 years old.</p>
+    </div>
+  )
+}
+
+export default AboutMe;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import Button from 'react-spatial/components/Button';
+
+import './Navbar.scss'
+
+class Navbar extends Component{
+  constructor(props){
+    super(props)
+    this.activeTab = undefined;
+    this.renderNavbarItems = this.renderNavbarItems.bind(this);
+    this.state = { activeTab: undefined };
+    this.tabClickHandler = this.tabClickHandler.bind(this)
+  }
+
+  tabClickHandler(selected) {
+    this.setState({ activeTab: selected });
+  };
+
+  renderNavbarItems(){
+    const items = ['Home', 'About Me', 'Projects', 'Experience', 'My Github']
+    return (
+      items.map((item, index) => {
+        return (
+          <Button
+            title={item}
+            active={index===this.state.activeTab}
+            tabIndex={index}
+            className={`navbar-item ${index===this.state.activeTab ? ' active' : ''}`}
+            onClick={(item) => this.tabClickHandler(index)}>{item}</Button>
+        )
+      })
+    )
+  }
+
+  render(){
+    return (
+      <div className='navbar'>
+        {this.renderNavbarItems()}
+      </div>
+    )
+  }
+
+}
+
+export default Navbar;
