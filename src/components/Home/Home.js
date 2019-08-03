12bab0204d1ef531920369f13502ba83e3ba2fce
danji90 <danji_ma90@hotmail.com>
2019-08-03 17:26:02 +0200

    finished hamburger menu

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -19,12 +19,28 @@ function Home() {
         <h1>Daniel Marsh-Hunn</h1>
         <h2>GI Researcher & Spatial Web Developer</h2>
         <div className="links-container">
-          <div><a title="LinkedIn" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/daniel-marsh-hunn-44097959/"><FaLinkedinIn size={25}/></a></div>
-          <div><a title="Github" target="_blank" rel="noopener noreferrer" href="https://github.com/danji90"><FaGithubAlt size={25}/></a></div>
-          <div><a title="Download CV" href="../../assets/images/MarshHunnDaniel_EuroCV.pdf" download><FaFilePdf size={25}/></a></div>
+          <div>
+            <a title="LinkedIn" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/daniel-marsh-hunn-44097959/">
+              <FaLinkedinIn size={25}/>
+            </a>
+          </div>
+          <div>
+            <a title="Github" target="_blank" rel="noopener noreferrer" href="https://github.com/danji90">
+              <FaGithubAlt size={25}/>
+            </a>
+          </div>
+          <div>
+            <a title="Download CV" href="../../assets/images/MarshHunnDaniel_EuroCV.pdf" download>
+              <FaFilePdf size={25}/>
+            </a>
+          </div>
         </div>
       </div>
-      <a href="mailto:danji_ma90@hotmail.com" className='mail-link'><Button title="Write e-mail" className="contact-button" ><FaPaperPlane size={20}/><strong>CONTACT ME</strong></Button></a>
+      <a href="mailto:danji_ma90@hotmail.com" className='mail-link'>
+        <Button title="Write e-mail" className="contact-button" onClick={()=>{}}>
+          <FaPaperPlane size={20}/><strong>CONTACT ME</strong>
+        </Button>
+      </a>
     </div>
   )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,7 +9,10 @@ class Navbar extends Component{
     super(props)
     this.activeTab = undefined;
     this.renderNavbarItems = this.renderNavbarItems.bind(this);
-    this.state = { activeTab: undefined };
+    this.state = {
+      activeTab: undefined,
+      navMenuOpen: false,
+    };
     this.tabClickHandler = this.tabClickHandler.bind(this)
   }
 
@@ -17,16 +20,22 @@ class Navbar extends Component{
     this.setState({ activeTab: selected });
   };
 
+  toggleExpandMenu() {
+    const { navMenuOpen } = this.state;
+    this.setState({navMenuOpen: !navMenuOpen})
+  }
+
   renderNavbarItems(){
     const items = ['Home', 'About Me', 'Projects', 'Experience', 'My Github']
+    const { activeTab } = this.state;
     return (
       items.map((item, index) => {
         return (
           <Button
+            key={index}
             title={item}
-            active={index===this.state.activeTab}
-            tabIndex={index}
-            className={`navbar-item ${index===this.state.activeTab ? ' active' : ''}`}
+            active={index===activeTab}
+            className={`navbar-item ${index===activeTab ? ' active' : ''}`}
             onClick={(item) => this.tabClickHandler(index)}>{item}
           </Button>
         )
@@ -35,14 +44,15 @@ class Navbar extends Component{
   }
 
   render(){
+    const { navMenuOpen } = this.state;
     return (
       <div className='navbar'>
-        <div className='nav-items'>
+        <Button title={'Expand'} className="menu-btn" tabIndex={-1} onClick={() => this.toggleExpandMenu()}>
+          <FaBars size={20}/ >
+        </Button>
+        <div className={`nav-items ${navMenuOpen ? '' : 'hidden'}`}>
           {this.renderNavbarItems()}
         </div>
-        <Button title={'Expand'} className="hamburger" tabIndex={-1} onClick={() => alert('expand')}>
-          <FaBars/ >
-        </Button>
       </div>
     )
   }
